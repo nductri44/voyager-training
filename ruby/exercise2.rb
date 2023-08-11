19142d60404b91d04a1b3b3946df90d5a118119a
nductri44 <nductri44@gmail.com>
2023-08-11 16:16:54 +0700

    Ruby Lesson 02: Choosing option practice

--- ruby/exercise2.rb
+++ ruby/exercise2.rb
@@ -7,22 +7,22 @@ op = gets.chomp
 
 case op
 when "1"
-    puts "Tinh dien tich tam giac::"
-    puts "Nhap chieu cao:"
-    a = gets.chomp.to_f
-    puts "Nhap canh day:"
-    h = gets.chomp.to_f
-    puts "Dien tich: #{(0.5)*a*h}"
+  puts "Tinh dien tich tam giac:"
+  puts "Nhap chieu cao:"
+  a = gets.chomp.to_f
+  puts "Nhap canh day:"
+  h = gets.chomp.to_f
+  puts "Dien tich: #{(0.5)*a*h}"
 when "2"
-    puts "Tinh dien tich hinh tron:"
-    puts "Nhap ban kinh:"
-    a = gets.chomp.to_f
-    puts "Dien tich: #{(3.14)*r*r}"
+  puts "Tinh dien tich hinh tron:"
+  puts "Nhap ban kinh:"
+  a = gets.chomp.to_f
+  puts "Dien tich: #{(3.14)*r*r}"
 when "3"
-    puts "Tinh dien tich hinh chu nhat:"
-    puts "Nhap chieu dai:"
-    a = gets.chomp.to_f
-    puts "Nhap canh rong:"
-    b = gets.chomp.to_f
-    puts "Dien tich: #{a*b}"
+  puts "Tinh dien tich hinh chu nhat:"
+  puts "Nhap chieu dai:"
+  a = gets.chomp.to_f
+  puts "Nhap canh rong:"
+  b = gets.chomp.to_f
+  puts "Dien tich: #{a*b}"
 end

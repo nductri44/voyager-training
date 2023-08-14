eef9845785e4fae720e49d4ffcffaf92ee109b9b
nductri44 <nductri44@gmail.com>
2023-08-14 16:26:07 +0700

    Ruby Lesson 02: OOP practice

---
+++ ruby/day02/exercise2/book.rb
@@ -0,0 +1,22 @@
+require './document.rb'
+
+class Book < Document
+  attr_accessor :author, :page
+
+  def initialize(id, publisher, release, price, author, page)
+    super(id, publisher, release, price)
+    @author = author
+    @page = page
+  end
+
+  def fee(rent_hour)
+    super
+  end
+
+  def print_info
+    super
+    puts "#{author}"
+    puts "#{page}"
+  end
+
+end

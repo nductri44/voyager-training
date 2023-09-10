5bcb337024fda5b7757945c298d72fbbc10ddf4e
nductri44 <nductri44@gmail.com>
2023-09-10 07:51:13 +0700

    update Ruby Day08: IO, CSV, JSON

--- ruby/day08/exercise3/exercise3.rb
+++ ruby/day08/exercise3/exercise3.rb
@@ -3,18 +3,7 @@ require 'json'
 
 file = File.read('exercise3.json')
 data_hash = JSON.parse(file)
-hashes = data_hash.values
-column_names = hashes.first.keys
-s =
-  CSV.generate do |csv|
-    csv << column_names
-    hashes.each do |x|
-      csv << x.values
-    end
-  end
 
-File.write('exercise3.csv', s)
-
-colon = proc { |header| header + ': ' }
-info = CSV.parse(File.open('exercise3.csv'), headers: true, header_converters: colon)
-puts info
+data_hash.each do |_key, value|
+  puts "Name: #{value['name']}, Salary: #{value['salary']}, Married: #{value['married']}"
+end
